2893972f5cddad444d9563f39f26a098c06d070a
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-03 19:22:22 +0700

    Finish temperature conversion exercise

--- The Odin Project/javascript-exercises/tempConversion/tempConversion.js
+++ The Odin Project/javascript-exercises/tempConversion/tempConversion.js
@@ -1,12 +1,20 @@
-const ftoc = function() {
+const roundNumber = (number) => Math.round(number * 10) / 10;
 
-}
+const ftoc = function (temperature) {
+  const ratio = 5 / 9;
+  const celcius = (temperature - 32) * ratio;
 
-const ctof = function() {
+  return roundNumber(celcius);
+};
 
-}
+const ctof = function (temperature) {
+  const ratio = 9 / 5;
+  const farenheit = temperature * ratio + 32;
+
+  return roundNumber(farenheit);
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
