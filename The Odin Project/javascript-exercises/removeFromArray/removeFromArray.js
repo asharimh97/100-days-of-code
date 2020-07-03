07c511ac65e42669f577eecb70e52c44110ce258
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-03 19:00:02 +0700

    Work on removeFromArray exercise

--- The Odin Project/javascript-exercises/removeFromArray/removeFromArray.js
+++ The Odin Project/javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  let [array, ...removedValues] = args;
+  removedValues.forEach((val) => {
+    const idx = array.indexOf(val);
+    if (idx >= 0) array.splice(idx, 1);
+  });
 
-}
+  return array;
+};
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
