da44612a75652095b14649684a6f73590538fa15
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-03 19:09:37 +0700

    Work on sumAll exercise

--- The Odin Project/javascript-exercises/sumAll/sumAll.js
+++ The Odin Project/javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function () {
+  const numbers = [...arguments];
+  const validNumbers = numbers.every(
+    (num) => num > 0 && typeof num === "number"
+  );
 
-}
+  if (!validNumbers) {
+    return "ERROR";
+  } else {
+    let result = 0;
+    let min = Math.min(numbers[0], numbers[1]);
+    let max = Math.max(numbers[0], numbers[1]);
 
-module.exports = sumAll
+    for (let i = min; i <= max; i++) {
+      result += i;
+    }
+
+    return result;
+  }
+};
+
+module.exports = sumAll;
