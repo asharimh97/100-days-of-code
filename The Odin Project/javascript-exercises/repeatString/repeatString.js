e36c74fc79c80b28aa9750e77d4ce936694f2edc
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-03 18:35:39 +0700

    Update repeatedString exercise

--- The Odin Project/javascript-exercises/repeatString/repeatString.js
+++ The Odin Project/javascript-exercises/repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (word, iteration) {
+  let repeatedWord = "";
+  if (iteration < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < iteration; i++) {
+      repeatedWord += word;
+    }
 
-}
+    return repeatedWord;
+  }
+};
 
-module.exports = repeatString
+module.exports = repeatString;
