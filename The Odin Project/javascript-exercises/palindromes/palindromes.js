c881bcd279c1c1dff843c1343d50c3b8428cafb1
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-06 21:37:10 +0700

    Finish palindrome task

--- The Odin Project/javascript-exercises/palindromes/palindromes.js
+++ The Odin Project/javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function (word) {
+  let newWord = "";
+  for (let j = 0; j < word.length; j++) {
+    if (/[a-z]/i.test(word[j])) {
+      newWord += word[j].toLowerCase();
+    }
+  }
 
-}
+  const mid = Math.round(newWord.length / 2);
+  const minus = newWord.length % 2 === 0 ? 0 : 1;
+  const leftPart = newWord.slice(0, mid - minus);
+  const rightPart = newWord.slice(mid).split("").reverse().join("");
 
-module.exports = palindromes
+  return leftPart === rightPart;
+};
+
+module.exports = palindromes;
