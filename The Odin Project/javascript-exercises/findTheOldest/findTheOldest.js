b785f6f3c259882a7284293347fef508cd206f2e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-06 22:38:37 +0700

    Finish findTheOldest task

--- The Odin Project/javascript-exercises/findTheOldest/findTheOldest.js
+++ The Odin Project/javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+  let oldest,
+    oldestAge = 0;
+  people.forEach((person) => {
+    const death = person.yearOfDeath || new Date().getFullYear();
+    const age = death - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldest = person;
+      oldestAge = age;
+    }
+  });
 
-}
+  return oldest;
+};
 
-module.exports = findTheOldest
+module.exports = findTheOldest;
