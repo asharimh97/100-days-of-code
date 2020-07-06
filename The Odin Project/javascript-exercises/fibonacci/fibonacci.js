e1acaa6f8350594b16e8a47cb5d48e6487b34757
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-06 22:27:47 +0700

    Finish fibonacci task

--- The Odin Project/javascript-exercises/fibonacci/fibonacci.js
+++ The Odin Project/javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (length) {
+  const len = parseInt(length);
+  if (len < 0) return "OOPS";
+  let res = [];
+  for (let i = 0; i < len; i++) {
+    if (i === 0 || i === 1) res.push(1);
+    else res.push(res[i - 1] + res[i - 2]);
+  }
 
-}
+  return res[len - 1];
+};
 
-module.exports = fibonacci
+module.exports = fibonacci;
