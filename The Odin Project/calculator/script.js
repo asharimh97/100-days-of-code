dae626a0421c93aaff1a4722c1e756ba2aae6264
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-10 21:57:46 +0700

    Add button function to calculator

--- The Odin Project/calculator/script.js
+++ The Odin Project/calculator/script.js
@@ -1,3 +1,69 @@
+// variables to operate calculation
+let sequence = "";
+let currentNumber = "";
+let currentOperator = "";
+
+// element selector
+const buttons = document.querySelectorAll(".button-pad");
+const display = document.querySelector(".calculator__display");
+const operator = document.querySelector(".calculator__operator");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const { type } = this.dataset;
+    if (type === "number") {
+      clickNumberPad(this);
+    } else if (type === "operator") {
+      clickOperatorPad(this);
+    }
+  });
+});
+
+function clickNumberPad(element) {
+  /**
+   * TODOS:
+   * if (currentNumber && currentOperator)
+   *    push number and operator to sequence
+   *    clean currentNumber
+   *
+   * push value to current number
+   * display number
+   */
+
+  if (currentOperator) {
+    sequence += ` ${currentOperator} `;
+    currentOperator = "";
+    display.textContent = "";
+  }
+
+  currentNumber += element.value;
+  display.textContent = currentNumber;
+}
+
+function clickOperatorPad(element) {
+  if (currentNumber) {
+    sequence += currentNumber;
+    currentNumber = "";
+  }
+
+  if (element.value === "=") {
+    // calculate sequence and push res to sequence
+    // clear operator and set current number and text content
+    console.log(sequence);
+    const [res] = calculate(sequence);
+
+    sequence = res;
+    currentNumber = "";
+    currentOperator = "";
+
+    operator.textContent = "";
+    display.textContent = res;
+    return;
+  }
+  currentOperator = element.value;
+  operator.textContent = currentOperator;
+}
+
 // operational function
 
 function add(a, b) {
@@ -17,7 +83,6 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-  console.log({ a, b, operator });
   const operators = {
     x: multiply,
     "*": multiply,
@@ -54,12 +119,12 @@ function cleanOperator(sequenceNumber, operand1, operand2) {
   return numbers;
 }
 
-function calculate() {
-  let numbers = "3 / 17 * 2 + 17";
+function calculate(sequence) {
+  let numbers = sequence;
   numbers = numbers.split(" ");
 
   numbers = cleanOperator(numbers, "*", "/");
   numbers = cleanOperator(numbers, "+", "-");
 
-  console.log(numbers);
+  return numbers;
 }
