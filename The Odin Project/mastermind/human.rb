baf13a9195c596c90d56b34c461f39e7dd83a959
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-25 09:31:02 +0700

    Update human create code method

--- The Odin Project/mastermind/human.rb
+++ The Odin Project/mastermind/human.rb
@@ -13,8 +13,19 @@ class Human < Player
 
   def create_code
     puts "Create your color code..."
+    puts "Consist of 4 color, e.g. red blue green yellow"
+    puts "Color available #{$COLORS.join(" ")}"
+
     code = gets.chomp.split(" ")
 
-    code
+    convert_code_to_map(code)
+  end
+
+  def convert_code_to_map(colors)
+    @color_code = colors
+    @color_code_map = colors.reduce({}) do |map, color|
+      map.has_key?(color) ? map[color] += 1 : map[color] = 1
+      map
+    end
   end
 end
