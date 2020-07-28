91af0d607d7d42f5fb13bd13f64dadea880ffbcd
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-28 20:17:59 +0700

    Add google civic info service

--- The Odin Project/event-manager/lib/event_manager.rb
+++ The Odin Project/event-manager/lib/event_manager.rb
@@ -1,7 +1,26 @@
 require "csv"
+require "google/apis/civicinfo_v2"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: "country",
+      roles: ["legislatorUpperBody", "legislatorLowerBody"]
+    )
+    legislators = legislators.officials
+    legislators_name = legislators.map(&:name).join(", ")
+    legislators_name
+  rescue
+    "Oops, Your zipcode is invalid brader!"
+  end
 end
 
 puts "Event Manager Initialized!"
@@ -11,6 +30,7 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
   name = "#{row[:first_name]} #{row[:last_name]}"
   zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "Name: #{name}, Zipcode: #{zipcode}"
+  puts "Name: #{name}, Zipcode: #{zipcode}, Legislator: #{legislators}"
 end
