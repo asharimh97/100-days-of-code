77dfacdf01a6f2ba56c7adc8e10c267c485640b4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-28 19:46:35 +0700

    Add zipcode and clean zipcode

--- The Odin Project/event-manager/lib/event_manager.rb
+++ The Odin Project/event-manager/lib/event_manager.rb
@@ -1,9 +1,22 @@
 require "csv"
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    "00000"
+  elsif zipcode.length < 5
+    zipcode.rjust 5, "0"
+  else
+    zipcode[0..4]
+  end
+end
+
 puts "Event Manager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
   name = "#{row[:first_name]} #{row[:last_name]}"
-  puts "Name: #{name}"
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "Name: #{name}, Zipcode: #{zipcode}"
 end
