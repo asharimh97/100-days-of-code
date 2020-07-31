5dc8e71cf520c24142d9a4d0f0ef64d174e44a6d
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-07-31 21:18:40 +0700

    Continue hangman project

--- The Odin Project/2-ruby-programming/hangman/main.rb
+++ The Odin Project/2-ruby-programming/hangman/main.rb
@@ -1,18 +1,49 @@
-dictionaries = "./assets/5desk.txt"
-$keywords = File.readlines dictionaries
+class Hangman
+  DICTIONARY_PATH = "./assets/5desk.txt"
+  @keywords = File.readlines DICTIONARY_PATH
 
-def select_word
-  keywords = $keywords.select { |x| x.length.between?(5, 12) }
-  total_keywords = keywords.length
+  def initialize
+    greeting
+  end
 
-  keywords[rand(total_keywords)].strip
+  def greeting
+    puts "-----------------------------"
+    puts "#                           #" 
+    puts "#  Welcome to Hangman game  #"
+    puts "#                           #"
+    puts "-----------------------------"
+    puts "\n"
+    
+    puts " |---|"
+    puts " |   O"
+    puts " |  /|\\"
+    puts " |   |"
+    puts " |  / \\"
+    puts " |"
+    puts "_|___|"
+    puts "\n"
+
+    puts "[1] Start new game"
+    puts "[2] Load game"
+  end
+
+  def select_word
+    keywords = @keywords.select { |x| x.length.between?(5, 12) }
+    total_keywords = keywords.length
+
+    @secret_word = keywords[rand(total_keywords)].strip
+    puts generate_stripes(@secret_word)
+  end
+
+  def generate_stripes(word)
+    word.gsub(/[\w]/, "_")
+  end
 end
 
-def generate_stripes(word)
-  word.gsub(/[\w]/, "_")
-end
+word = Hangman.new
 
-word = select_word()
+# word = select_word()
 
-puts word
-puts generate_stripes(word)
+# puts word
+# puts generate_stripes(word)
+# puts print_greeting()
