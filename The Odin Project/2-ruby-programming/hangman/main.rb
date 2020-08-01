f49c7e187e3c24e5a082e59b00552d96e17f4114
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-08-01 12:21:24 +0700

    Add methods to allow player play hangman game

--- The Odin Project/2-ruby-programming/hangman/main.rb
+++ The Odin Project/2-ruby-programming/hangman/main.rb
@@ -1,9 +1,102 @@
 class Hangman
   DICTIONARY_PATH = "./assets/5desk.txt"
-  @keywords = File.readlines DICTIONARY_PATH
+  $keywords = File.readlines DICTIONARY_PATH
 
   def initialize
+    # store guessed chars
+    @guessed_chars = []
+    @correct_chars = ""
+    @game_over = false
+
     greeting
+
+    choice = gets.chomp
+
+    case choice
+    when "1"
+      puts "Let's start the game. The secret word is:"
+      puts "\n"
+      start_game
+    when "2"
+      puts "Load save game"
+    else
+      puts "Uh oh, answer is invalid"
+    end
+  end
+
+  def start_game
+    x = 0
+    select_word
+    while (@chances > 0 && @game_over == false) do
+      if x > 0
+        puts @striped_word
+        display_remaining_chances 
+        puts "Guessed chars: #{@guessed_chars.join(", ")}"
+      end
+      guess_word
+
+      # regenerate striped word
+      generate_stripes
+
+      check_answer
+      x += 1
+    end
+
+    announce_result
+  end
+
+  def guess_word
+    guess = gets.chomp[0]
+
+    unless @guessed_chars.include? (guess)
+      if @secret_word.include? (guess)
+        @correct_chars += guess
+      end
+      @guessed_chars.push(guess)
+      @chances -= 1
+    else
+      puts "Chars has been used, can't be reused"
+    end
+    puts 
+
+  end
+
+  def check_answer
+    @game_over = @striped_word == @secret_word
+  end
+
+  def save_game
+  end
+
+  def load_game
+    # set secret word and display stripes
+    # set 
+  end
+
+  def select_word
+    keywords = $keywords.select { |x| x.length.between?(5, 12) }
+    total_keywords = keywords.length
+
+    @secret_word = keywords[rand(total_keywords)].strip
+    @chances = @secret_word.length
+    puts @secret_word
+    puts generate_stripes
+    display_remaining_chances
+  end
+
+  def generate_stripes
+    word = @secret_word
+    @striped_word = word.gsub(/[\w]/, "_")
+    
+    @striped_word = word.gsub(/[^#{@correct_chars}]/, "_") if @correct_chars.length > 0
+
+    @striped_word
+  end
+
+  # non technical methods lay here
+
+  def display_remaining_chances
+    puts "You have #{@chances} chance(s) to guess"
   end
 
   def greeting
@@ -14,6 +107,13 @@ class Hangman
     puts "-----------------------------"
     puts "\n"
     
+    display_hangman
+
+    puts "[1] Start new game"
+    puts "[2] Load game"
+  end
+
+  def display_hangman
     puts " |---|"
     puts " |   O"
     puts " |  /|\\"
@@ -22,28 +122,16 @@ class Hangman
     puts " |"
     puts "_|___|"
     puts "\n"
-
-    puts "[1] Start new game"
-    puts "[2] Load game"
   end
 
-  def select_word
-    keywords = @keywords.select { |x| x.length.between?(5, 12) }
-    total_keywords = keywords.length
-
-    @secret_word = keywords[rand(total_keywords)].strip
-    puts generate_stripes(@secret_word)
-  end
-
-  def generate_stripes(word)
-    word.gsub(/[\w]/, "_")
+  def announce_result 
+    if @game_over
+      puts @secret_word
+      puts "You guessed it right on the spot!" 
+    else
+      puts "Too bad! The correct answer is #{@secret_word}"
+    end
   end
 end
 
 word = Hangman.new
-
-# word = select_word()
-
-# puts word
-# puts generate_stripes(word)
-# puts print_greeting()
