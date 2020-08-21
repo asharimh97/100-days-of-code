66947f8633a7ce6c5cef5829c2c74b75ed8f88c0
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-08-21 21:57:08 +0700

    Add movement generator for chess pieces

---
+++ The Odin Project/2-ruby-programming/chess/lib/pieces/movements.rb
@@ -0,0 +1,31 @@
+module Movements
+  KNIGHT_MOVES = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, 2], [-2, 1], [-2, -1], [-1, -2]]
+  
+  def Movements.ROOK_MOVES
+    rook = []
+    for i in 1..7
+      rook.unshift([0, -i])
+      rook.push([i, 0])
+      rook.push([0, i])
+      rook.unshift([-i, 0])
+    end
+
+    rook
+  end
+
+  def Movements.BISHOP_MOVES
+    bishop = []
+    for i in 1..7
+      bishop.unshift([-i, -i])
+      bishop.unshift([-i, i])
+      bishop.push([i, i])
+      bishop.unshift([i, -i])
+    end
+
+    bishop
+  end
+end
+
+# p Movements::KNIGHT_MOVES
+# # p Movements::ROOK_MOVES()
+# p Movements::BISHOP_MOVES()
