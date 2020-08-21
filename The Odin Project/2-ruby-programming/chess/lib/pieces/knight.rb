c25306b12f611d31e474829bebc3b8e7c24466b0
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-08-21 22:00:16 +0700

    Add possible move for piece rook and knight

--- The Odin Project/2-ruby-programming/chess/lib/pieces/knight.rb
+++ The Odin Project/2-ruby-programming/chess/lib/pieces/knight.rb
@@ -1,7 +1,24 @@
+require_relative "movements"
+
 class Knight
   attr_accessor :color
-  def initialize(@color)
+  def initialize(color)
     @color = color
+  end
+
+  def possible_moves (position)
+    moves = []
+    x, y = position
+
+    Movements::KNIGHT_MOVES.each do |move|
+      i, j = move
+      x1 = x + i
+      y1 = y + i
+
+      moves.push([x1, y1]) if x1.between?(0, 7) && y1.between?(0,7)
+    end
+
+    moves
   end
 
   def to_s
@@ -16,3 +33,7 @@ class Knight
     to_s
   end
 end
+
+knight = Knight.new("black")
+
+p knight.possible_moves([0,1])
--- The Odin Project/2-ruby-programming/chess/lib/pieces/rook.rb
+++ The Odin Project/2-ruby-programming/chess/lib/pieces/rook.rb
@@ -1,7 +1,24 @@
+require_relative "./movements"
+
 class Rook 
   attr_accessor :color
   def initialize (color)
     @color = color
+  end
+
+  def possible_moves (position)
+    moves = []
+    x, y = position
+
+    Movements::ROOK_MOVES().each do |move|
+      i, j = move
+      x1 = x + i
+      y1 = y + j
+
+      moves.push([x1, y1]) if x1.between?(0, 7) && y1.between?(0, 7)
+    end
+
+    moves
   end
 
   def to_s
@@ -12,3 +29,7 @@ class Rook
     to_s
   end
 end
+
+rook = Rook.new("black")
+
+p rook.possible_moves([0, 0])
