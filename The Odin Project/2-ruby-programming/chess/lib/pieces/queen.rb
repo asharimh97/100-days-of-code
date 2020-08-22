d421fea180102946a21352b88f39b4763eb4f5ef
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-08-22 19:13:31 +0700

    Implement queen moves

--- The Odin Project/2-ruby-programming/chess/lib/pieces/queen.rb
+++ The Odin Project/2-ruby-programming/chess/lib/pieces/queen.rb
@@ -1,7 +1,25 @@
+require_relative "./movements"
+
 class Queen
   attr_accessor :color
   def initialize (color)
     @color = color
+  end
+
+  def possible_moves (position)
+    queen_moves = [].concat(Movements.ROOK_MOVES, Movements.BISHOP_MOVES())
+    moves = []
+    x, y = position
+
+    queen_moves.each do |move|
+      i, j = move
+      x1 = x + i
+      y1 = y + j
+
+      moves.push([x1, y1]) if x1.between?(0,7) && y1.between?(0,7)
+    end
+
+    moves
   end
 
   def to_s
@@ -12,3 +30,7 @@ class Queen
     to_s
   end
 end
+
+queen = Queen.new("black")
+
+p queen.possible_moves([0, 4])
