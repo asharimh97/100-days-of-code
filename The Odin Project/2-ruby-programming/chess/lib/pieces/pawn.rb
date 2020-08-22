3b0f5d6b72b2d86b489bc167a7bdcd41688769dc
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-08-22 19:12:43 +0700

    Implement pawn moves

--- The Odin Project/2-ruby-programming/chess/lib/pieces/pawn.rb
+++ The Odin Project/2-ruby-programming/chess/lib/pieces/pawn.rb
@@ -2,6 +2,18 @@ class Pawn
   attr_accessor :color
   def initialize (color)
     @color = color
+    @direction = color == "black" ? -1 : 1
+  end
+
+  def possible_moves (position)
+    moves = []
+    x, y = position
+
+    moves << [x, y + @direction] if (y + @direction).between?(0, 7)
+
+    moves << [x, y + (@direction * 2)] if (@color == "black" && y == 6) || (@color == "white" && y == 1)
+    
+    moves
   end
 
   def to_s
@@ -12,3 +24,9 @@ class Pawn
     to_s
   end
 end
+
+pawn = Pawn.new("black")
+pawn2 = Pawn.new("white")
+
+p pawn.possible_moves([0, 6])
+p pawn2.possible_moves([0, 7])
