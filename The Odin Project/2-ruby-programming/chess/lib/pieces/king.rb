3da3479e0f3244d045040c65ddd0cc96477a9494
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-08-22 19:24:40 +0700

    Implement king moves

--- The Odin Project/2-ruby-programming/chess/lib/pieces/king.rb
+++ The Odin Project/2-ruby-programming/chess/lib/pieces/king.rb
@@ -1,7 +1,24 @@
+require_relative "./movements"
+
 class King
   attr_accessor :color
   def initialize (color)
     @color = color
+  end
+
+  def possible_moves (position)
+    moves = []
+    x, y = position
+
+    Movements::KING_MOVES.each do |move|
+      i, j = move
+      x1 = x + i
+      y1 = y + j
+
+      moves.push([x1, y1]) if x1.between?(0, 7) && y1.between?(0, 7)
+    end
+
+    moves
   end
 
   def to_s
@@ -12,3 +29,7 @@ class King
     to_s
   end
 end
+
+king = King.new("black")
+
+p king.possible_moves([3, 0])
