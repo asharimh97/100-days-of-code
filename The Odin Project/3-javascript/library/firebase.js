ff31cb100b59eb2a8a2a5ed0fc0ed64c5df1157f
Ashari Muhammad H <asharimh97@gmail.com>
2020-09-07 19:14:13 +0700

    Initiate firebase

---
+++ The Odin Project/3-javascript/library/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB-cw50Td8musuyqxYudIbPrmQETNO32hA",
+  authDomain: "library-d1c07.firebaseapp.com",
+  databaseURL: "https://library-d1c07.firebaseio.com",
+  projectId: "library-d1c07",
+  storageBucket: "library-d1c07.appspot.com",
+  messagingSenderId: "345646603759",
+  appId: "1:345646603759:web:4e8662625cd4727a6aea1a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
