6286a27e27de095dbc48240d4dff376ef800f3f9
Ashari Muhammad H <asharimh97@gmail.com>
2020-09-07 19:13:58 +0700

    Finished implement library without storing

--- The Odin Project/3-javascript/library/library.js
+++ The Odin Project/3-javascript/library/library.js
@@ -1,30 +1,30 @@
 let myLibrary = [
   {
     id: 1,
-    title: "Lorem Ipsum",
-    author: "Ipsum",
-    pages: 200,
+    title: "Think and Grow Rich",
+    author: "Napoleon Hill",
+    pages: 265,
     read: false
   },
   {
     id: 2,
-    title: "Lorem Ipsum",
-    author: "Ipsum",
-    pages: 200,
+    title: "Barking Up the Wrong Tree",
+    author: "Eric Barker",
+    pages: 305,
     read: false
   },
   {
     id: 3,
-    title: "Lorem Ipsum",
-    author: "Ipsum",
-    pages: 200,
+    title: "Everything is Fucked",
+    author: "Mark Manson",
+    pages: 292,
     read: false
   },
   {
     id: 4,
-    title: "Lorem Ipsum",
+    title: "It Doesn't Have to be Crazy at Work",
     author: "Ipsum",
-    pages: 200,
+    pages: 270,
     read: false
   }
 ];
@@ -47,6 +47,7 @@ function addBookToLibrary(data) {
 // DOM FUNCTIONS
 
 const form = document.querySelector("#form-add-book");
+const table = document.querySelector("#table-list-book tbody");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -58,5 +59,54 @@ form.addEventListener("submit", function (e) {
     read: JSON.parse(read.value)
   });
 
-  console.table(myLibrary);
+  form.reset();
+
+  // console.table(myLibrary);
+
+  generateTable();
 });
+
+function generateTable() {
+  table.innerHTML = "";
+  myLibrary.forEach(book => {
+    const element = `
+      <tr>
+        <td>${book.id}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read ? "Read" : "Not read yet"}</td>
+        <td>
+          <button onclick='deleteBook(${book.id})'>Del!</button>
+          <button onclick='toggleRead(${book.id})'>${
+      book.read ? "Unread?" : "Read?"
+    }</button>
+        </td>
+      </tr>
+    `;
+
+    table.innerHTML += element;
+  });
+}
+
+function deleteBook(id) {
+  const bookId = myLibrary.findIndex(book => book.id == id);
+  myLibrary.splice(bookId, 1);
+
+  generateTable();
+}
+
+function toggleRead(id) {
+  const book = myLibrary.find(book => book.id == id);
+  const bookId = myLibrary.findIndex(book => book.id == id);
+  let newBook = {
+    ...book,
+    read: !book.read
+  };
+
+  myLibrary.splice(bookId, 1, newBook);
+
+  generateTable();
+}
+
+generateTable();
