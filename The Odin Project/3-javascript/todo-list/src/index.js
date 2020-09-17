1a7e35434bee018cd1364cd27e61b7576b824694
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-09-17 19:53:31 +0700

    Implement some functions

--- The Odin Project/3-javascript/todo-list/src/index.js
+++ The Odin Project/3-javascript/todo-list/src/index.js
@@ -1,7 +1,9 @@
-const dom = require("./dom");
+const { DOMManipulator, closeModal } = require("./dom");
 const storage = require("./storage");
 const Project = require("./projects");
 const Todo = require("./todo");
+
+DOMManipulator.resetTodoInPage();
 
 // first load storage
 // if found storage, then ok
@@ -16,16 +18,32 @@ if (!projects) {
 
 // load all projects to sidebar
 projects.forEach(project => {
-  dom.DOMManipulator.addProjectToSidebar(project);
+  DOMManipulator.addProjectToSidebar(project);
 });
 // add sidebar event listener
 
 // if user click project
 // get project data by project name
 // render project title, description, and all todos
+// reset all todos first
 
 // view detail project
 
 // add project...
+// form
+const form = document.querySelector("#form__todo");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let data = new FormData(form);
+  data = Object.fromEntries(data);
+  data.dueDate = new Date(data.date);
+  const todo = Todo(data);
+
+  DOMManipulator.addTodoToPage(todo);
+
+  form.reset();
+  closeModal("modal__todo--add");
+});
 
 // edit project
