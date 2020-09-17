9e99e775ea688b3a903c79a78b703d5d69ac4836
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-09-17 19:53:15 +0700

    Add dom manipulator to add todo to page

--- The Odin Project/3-javascript/todo-list/src/dom.js
+++ The Odin Project/3-javascript/todo-list/src/dom.js
@@ -1,29 +1,88 @@
+const { format } = require("date-fns");
+
 const buttonAdd = document.querySelector(".button__todo--add");
 const modalAdd = document.querySelector("#modal__todo--add");
 const modalClosers = document.querySelectorAll(".modal__closer");
+const buttonCancel = document.querySelectorAll(".button__modal--cancel");
 
 buttonAdd.addEventListener("click", () => {
   modalAdd.classList.add("modal--show");
 });
 
-modalClosers.forEach(closer => {
-  closer.addEventListener("click", function () {
-    const { target } = this.dataset;
-    const modal = document.querySelector(`#${target}`);
+export function closeModal(modalTarget) {
+  const target = modalTarget ? modalTarget : this.dataset.target;
+  const modal = document.querySelector(`#${target}`);
 
-    if (modal.classList.contains("modal--show")) {
-      modal.classList.remove("modal--show");
-    }
-  });
+  if (modal.classList.contains("modal--show")) {
+    modal.classList.remove("modal--show");
+  }
+}
+
+modalClosers.forEach(closer => {
+  closer.addEventListener("click", closeModal);
+});
+
+buttonCancel.forEach(btn => {
+  btn.addEventListener("click", closeModal);
 });
 
 const sidebar = document.querySelector("#sidebar__navs");
+const list = document.querySelector("#todo-list");
 
 export const DOMManipulator = {
   // todo dom
-  addTodoToPage: () => {},
+  addTodoToPage: todo => {
+    const todoContent = `
+      <div class="todo-item__body">
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          class="todo-item__checkbox"
+        />
+        <div class="todo-item__content">
+          <p class="todo-item__title">${todo.title}</p>
+          <p class="todo-item__date">${format(
+            todo.dueDate,
+            "EEEE, d MMM YYY"
+          )}</p>
+        </div>
+      </div>
+      <ion-icon name="flag" class="todo-item__flag ${
+        todo.priority && `todo-item__flag--${todo.priority}`
+      }"></ion-icon>
+    `;
+
+    const todoItem = document.createElement("div");
+    todoItem.className = "todo-item";
+    todoItem.innerHTML = todoContent;
+
+    const checkbox = todoItem.querySelector("input");
+    todoItem.addEventListener(
+      "click",
+      function (e) {
+        if (e.target != checkbox) {
+          // buka modal edit
+          console.log(todo);
+          return;
+        }
+      },
+      true
+    );
+    checkbox.addEventListener("change", e => {
+      e.cancelBubble = true;
+      todoItem.classList.toggle("todo-item--complete");
+      todo.setTodo({ ...todo, complete: !todo.complete });
+      console.log("click checkbox");
+    });
+
+    list.appendChild(todoItem);
+  },
   removeTodoFromPage: () => {},
   updateTodoToPage: () => {},
+  resetTodoInPage: () => {
+    list.innerHTML = "";
+  },
 
   // sidebar project dom
   addProjectToSidebar: function (project) {
