9301bb5bb7480f7f0f14ebf0f85ce58843d674f6
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-09-17 19:52:59 +0700

    Add flag complete to todo

--- The Odin Project/3-javascript/todo-list/src/todo.js
+++ The Odin Project/3-javascript/todo-list/src/todo.js
@@ -1,26 +1,48 @@
 const Todo = (todo = {}) => {
   let { title, description, dueDate, priority } = todo;
   const id = Math.floor(Math.random() * 1000);
+  let complete = false;
 
   const setTodo = newTodo => {
     const {
       title: newTitle,
       description: desc,
       dueDate: newDate,
-      priority: newPriority
+      priority: newPriority,
+      complete: newComplete
     } = newTodo;
 
     title = newTitle;
     description = desc;
     dueDate = newDate;
     priority = newPriority;
+    complete = newComplete;
+  };
+
+  const setComplete = () => {
+    complete = !complete;
   };
 
   return {
     id,
+    complete,
     ...todo,
-    setTodo
+    setTodo,
+    setComplete
   };
 };
+
+// const t = Todo({
+//   title: "asd",
+//   description: "asd",
+//   dueDate: new Date(),
+//   priority: ""
+// });
+
+// console.log(t);
+
+// t.setComplete();
+
+// console.log(t);
 
 module.exports = Todo;
