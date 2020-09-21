61349c5ec8a91a9201a70fd65b9bb4371eb151bc
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-09-21 19:36:05 +0700

    Initiate react typescript project

---
+++ The Odin Project/3-javascript/todo-list-react/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  title: string;
+};
+
+const Sidebar: React.FC<Props> = ({ title }) => {
+  return <aside>Lorem ipsum</aside>;
+};
+
+export default Sidebar;
