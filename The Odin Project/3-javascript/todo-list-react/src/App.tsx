10c70b754b2491834fd8924ce40fb9ba1341c0e4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-09-22 20:25:52 +0700

    Put sidebar and main content to app component

--- The Odin Project/3-javascript/todo-list-react/src/App.tsx
+++ The Odin Project/3-javascript/todo-list-react/src/App.tsx
@@ -1,28 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import logo from "./logo.svg";
 import "./App.css";
 
 import Sidebar from "./components/Sidebar";
+import ProjectHeading from "./components/ProjectHeading";
+
+const Main = styled.main`
+  margin-left: 250px;
+  padding: 32px;
+  width: calc(100vw - 250px);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
       <Sidebar title={"lorem ipsum"} />
-    </div>
+      <Main>
+        <ProjectHeading
+          title={"General"}
+          description={"Lorem Ipsum dolorsit amet nunc puerta"}
+        />
+      </Main>
+    </>
   );
 }
 
