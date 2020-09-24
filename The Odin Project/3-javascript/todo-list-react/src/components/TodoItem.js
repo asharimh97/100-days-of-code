3259b4f8d8dc3f6a82777b2484e577ba24d5e9c5
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-09-24 20:24:11 +0700

    Add todo item component

---
+++ The Odin Project/3-javascript/todo-list-react/src/components/TodoItem.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { IoIosFlag } from "react-icons/io";
+import { format } from "date-fns";
+import theme from "./theme";
+
+const Wrapper = styled.div`
+  align-items: center;
+  background: ${props =>
+    props.completed ? theme.colors.shadedWhite : theme.colors.white};
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 12px;
+  padding: 4px;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background: ${theme.colors.shadedWhite};
+  }
+
+  &:last-child {
+    margin: 0;
+  }
+`;
+
+const TodoTitle = styled.p`
+  color: ${theme.colors.black};
+  margin-bottom: 4px;
+  margin-top: 0;
+`;
+
+const TodoDate = styled.p`
+  color: ${theme.colors.darkerGrey};
+  font-size: 0.85em;
+  margin: 0;
+`;
+
+const TodoBody = styled.div`
+  align-items: flex-start;
+  display: flex;
+  width: 100%;
+
+  ${TodoDate}, ${TodoTitle} {
+    text-decoration: ${props => props.completed && "line-through"};
+  }
+`;
+
+const TodoItem = ({ todo }) => {
+  const { title, date, priority } = todo;
+  const [completed, setCompleted] = useState(false);
+
+  const handleCompleteTodo = e => {
+    console.log("Complete this todo", todo);
+    setCompleted(!completed);
+  };
+
+  const handleClickTodo = e => {
+    console.log("Click body todo");
+  };
+
+  const PRIORITY_COLOR = {
+    high: theme.colors.red,
+    medium: theme.colors.orange,
+    low: theme.colors.green
+  };
+  return (
+    <Wrapper completed={completed}>
+      <TodoBody completed={completed}>
+        <input
+          type="checkbox"
+          onChange={handleCompleteTodo}
+          style={{ marginRight: "16px" }}
+        />
+        <div onClick={handleClickTodo} style={{ width: "100%" }}>
+          <TodoTitle>{title}</TodoTitle>
+          <TodoDate>{format(date, "d MMMM yyyy")}</TodoDate>
+        </div>
+      </TodoBody>
+      <IoIosFlag fill={PRIORITY_COLOR[priority] || theme.colors.softerGrey} />
+    </Wrapper>
+  );
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.object
+};
+
+TodoItem.defaultProps = {
+  todo: {
+    title: "Sample title",
+    date: new Date(),
+    priority: ""
+  }
+};
+
+export default TodoItem;
