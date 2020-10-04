176fa2493f7553263c8cc8f65cc205a59461a768
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-04 20:35:31 +0700

    Add page generator for blog post

--- free-practices/gatsby-site/gatsby-node.js
+++ free-practices/gatsby-site/gatsby-node.js
@@ -1,6 +1,7 @@
-exports.createPages = ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
 
+  // try parsing data from array
   const simpleDatas = [
     {
       title: "Simple data 1",
@@ -21,6 +22,32 @@ exports.createPages = ({ actions }) => {
       context: {
         data
       }
+    });
+  });
+
+  // parse data from blog posts file
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    console.error(result.errors);
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: require.resolve(`./src/templates/blog-template.js`)
     });
   });
 };
